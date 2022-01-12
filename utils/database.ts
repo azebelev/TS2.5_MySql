c8e43853121c2a0c049b2aaab4e055a63d2f7bf3
AZEBELEV <92332567+AZEBELEV@users.noreply.github.com>
2022-01-12 17:56:16 +0200

    1

---
+++ utils/database.ts
@@ -0,0 +1,15 @@
+import mysql from 'mysql2';
+const dbConfig = {
+  password: 'asd123',
+  user: 'root',
+  database: 'node-todo-min',
+  host: 'localhost',
+  port: 3306
+};
+const db = mysql.createConnection(dbConfig);
+
+
+
+
+
+export { dbConfig, db };
